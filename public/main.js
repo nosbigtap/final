2a4ffc5d58cd3e4893f47c59b017bd59d5dc9b59
Patrick Gibson <patrickgibson@Patricks-MacBook-Air.local>
2016-10-05 10:33:39 -0600

    fuck this fucking text editing FUCK

--- public/main.js
+++ public/main.js
@@ -18,11 +18,4 @@ for ( var i=0, len = draggableElems.length; i < len; i++ ) {
   draggies.push( draggie );
 }
 
-tinymce.init({
-        selector: "textarea",
-        width: 350,
-        height: 200,
-        statusbar: false,
-        menubar:false,
-        toolbar: "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | table | fontsizeselect"
-     })
+var editor = new MediumEditor('.editable');
