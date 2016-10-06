52835926a8391f3a2807270773b51474a65dc47b
Patrick Gibson <patrickgibson@Patricks-MacBook-Air.local>
2016-10-06 12:02:41 -0600

    git this shit to the hub

---
+++ public/js/module.stacksheet.js
@@ -0,0 +1,31 @@
+
+// multiple .draggable elements
+
+// get all draggie elements
+var draggableElems = document.querySelectorAll('.draggable');
+// array of Draggabillies
+var draggies = []
+// init Draggabillies
+for ( var i=0, len = draggableElems.length; i < len; i++ ) {
+  var draggableElem = draggableElems[i];
+  var draggie = new Draggabilly( draggableElem, {
+    handle: '.handle',
+    containment: true,
+    grid: [ 20, 20 ]
+  });
+  draggies.push( draggie );
+}
+
+//Pen
+var editor = new Pen(document.getElementById('editor'));
+
+// config
+ var options = {
+   // toolbar: document.getElementById('custom-toolbar'),
+   editor: document.querySelector('[data-toggle="pen"]'),
+   debug: true,
+   list: [
+     'insertimage', 'blockquote', 'h2', 'h3', 'p', 'code', 'insertorderedlist', 'insertunorderedlist', 'inserthorizontalrule',
+     'indent', 'outdent', 'bold', 'italic', 'underline', 'createlink'
+   ]
+ };
