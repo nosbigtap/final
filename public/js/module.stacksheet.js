1343b2db696e6e424f40c8aa03718ade6849b13c
Patrick Gibson <patrickgibson@Patricks-MacBook-Air.local>
2016-10-07 15:00:04 -0600

    :collision: working better, but still broken

--- public/js/module.stacksheet.js
+++ public/js/module.stacksheet.js
@@ -1,50 +1,48 @@
 angular.module('stacksheet', [])
-  .controller('textController', txtCtrl);
+  .controller('textController', textController);
 
-//add element (Draggable Textarea)
-function txtCtrl(){
-  var textAreas = this;
-  this.angular.element(
-    '<div class="draggable">
-      <div class="handle">
-      <textarea></textarea>
-      </div>
-    </div>'
-  )
-  });
-  var body = angular.element($document).find('body').eq(0);
-  body.append(this)
-}
 
 //
-// function txtCtrl(){
-//   newTextElem = document.createElement('textarea');
-//   newTextElem.className="draggable";
-//   newTextElem.className="handle";
-// };
-//
-// //remove element (Dragabble textarea)
-// function removeTxtElem(body) {
-//     // Removes an element from the document
-//     var element = document.getElementById('textarea');
-//     element.parentNode.removeChild(element);
-// };
+function textController(){
+    var txtCtrl = this;
+    txtCtrl.textAreas = [];
 
-// multiple .draggable elements
-// get all draggie elements
-var draggableElems = document.querySelectorAll('.draggable');
-// array of Draggabillies
-var draggies = []
-// init Draggabillies
-for ( var i=0, len = draggableElems.length; i < len; i++ ) {
-  var draggableElem = draggableElems[i];
-  var draggie = new Draggabilly( draggableElem, {
-    handle: '.handle',
-    containment: true,
-    grid: [ 20, 20 ]
-  });
-  draggies.push( draggie );
-}
+
+    // multiple .draggable elements
+    // get all draggie elements
+    var draggableElems = document.querySelectorAll('.draggable');
+    // array of Draggabillies
+    var draggies = []
+    // init Draggabillies
+    for ( var i=0, len = draggableElems.length; i < len; i++ ) {
+      var draggableElem = draggableElems[i];
+      var draggie = new Draggabilly( draggableElem, {
+        handle: '.handle',
+        containment: true,
+        grid: [ 20, 20 ]
+      });
+      draggies.push( draggie );
+    }
+
+    //add element
+    txtCtrl.addTextArea = function() {
+        console.info("Clicked addTextControl", txtCtrl);
+        txtCtrl.textAreas.push(' ');
+    };
+    // remove element
+    txtCtrl.removeTextArea = function() {
+        console.info("Clicked removeTextArea");
+    };
+
+    // save text info
+    txtCtrl.saveInfo = function(){
+       console.log('save shit')
+    };
+
+};
+
+
+
 
 // //Pen
 // var editor = new Pen(document.getElementById('editor'));
